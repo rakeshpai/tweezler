3ff9059fafb7cfcab10e50248401dc7caf999fe4
Rakesh Pai <rakeshpai@gmail.com>
2019-01-28 09:26:21 +0530

    Added user authentication

--- src/models/__tests__/users.js
+++ src/models/__tests__/users.js
@@ -1,5 +1,5 @@
 const { open, close, dropDB } = require('../mongoose/_connection');
-const { createUser, userByUsername } = require('../users');
+const { createUser, userByUsername, authenticate } = require('../users');
 
 beforeAll(open);
 afterAll(() => dropDB().then(close));
@@ -9,4 +9,14 @@ it('should create a user', async () => {
 
   const createdUser = await userByUsername('myNewTweezlerUserId');
   expect(createdUser.username).toEqual('myNewTweezlerUserId');
+});
+
+it('should authenticate a user', async () => {
+  await createUser('anotherUser', 'sAfEPaSsWoRd');
+
+  const isValid = await authenticate('anotherUser', 'sAfEPaSsWoRd');
+  expect(isValid).toEqual(true);
+
+  const isValid2 = await authenticate('anotherUser', 'invalid-password');
+  expect(isValid2).toEqual(false);
 });
--- src/models/users.js
+++ src/models/users.js
@@ -1,13 +1,22 @@
-const { hash } = require('bcrypt');
+const { hash, compare } = require('bcrypt');
 const User = require('./mongoose/users');
 
 const saltRounds = 10;
 
 const encrypt = password => hash(password, saltRounds);
 
-module.exports.createUser = async (username, password) => User.create({
+const createUser = async (username, password) => User.create({
   username,
   password: await encrypt(password),
 });
 
-module.exports.userByUsername = async username => User.findOne({ username });
+const userByUsername = async username => User.findOne({ username });
+
+const authenticate = async (username, password) => {
+  const user = await userByUsername(username);
+  return compare(password, user.password);
+};
+
+module.exports.createUser = createUser;
+module.exports.userByUsername = userByUsername;
+module.exports.authenticate = authenticate;
